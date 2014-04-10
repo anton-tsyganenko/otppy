9186e1f69cdb77e26cf73eec9534723950242a3c
Anton Tsyganenko <anton-tsyganenko@yandex.ru>
2014-04-10 20:07:04 +0400

    added comments to code

--- crypt.py
+++ crypt.py
@@ -5,29 +5,29 @@ decryption = False
 def sxor(str1, str2): # use xor for bytes
     return ''.join(chr(str1 ^ str2) for str1, str2 in zip(str1, str2))
 
-def validate_key(key, text):
+def validate_key(key, text): # don't let user to use small key
     if len(key) < len(text):
         print("the key must have the text length or be longer")
         exit()
     return key
 
-text = input("enter the text > ")
+text = input("enter the text > ") # input text, decide, if it's a encrypted text and convert to bytes
 try:
     text = bytes.fromhex(text.replace(" ", ""))
     decryption = True
 except:
     text = bytes(text, "utf-8")
 
-key = input("enter the key > ")
+key = input("enter the key > ") # input key, check, if it's hex
 try:
     key = bytes.fromhex(key.replace(" ", ""))
 except:
     print ("the key must be in HEX format")
     exit()
 
-result = str(sxor(text, validate_key(key, text)))
+result = str(sxor(text, validate_key(key, text))) # encrypt/decrypt the text
 
-if not decryption:
+if not decryption: encrypted result convert to hex format
     result = " ".join("{:02x}".format(ord(c)) for c in result)
 
 print (result)
