3827c9987efce24ab5394786a59a75ebd4ff19a1
Anton Tsyganenko <anton-tsyganenko@yandex.ru>
2014-04-10 18:18:48 +0400

    reworked the program: use xor for encryption and hex for output

--- crypt.py
+++ crypt.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
 
-try:
-  def encrypt(text, key):
-    i = 0
-    encrypted = ""
-    for t in text:
-      p = key[i]
-      i += 1
-      encrypted += str(ord(t)+ord(p)) + " "
-    return encrypted[:-1]
+decryption = False
 
-  def decrypt(text, key):
-    try:
-      i = 0
-      decrypted = ""
-      text = text.split(" ")
-      for t in text:
-        p = key[i]
-        i += 1
-        decrypted += chr(int(t)-ord(p))
-      return decrypted
-    except ValueError:
-        return "Ошибка! скорее всего вы ввели не последовательность чисел, разделенных пробелом."
+def sxor(str1, str2):
+    return ''.join(chr(ord(str1) ^ ord(str2)) for str1, str2 in zip(str1, str2))
 
-  def resize_key(key, text):
+def resize_key(key, text):
     while True:
       if len(text) > len(key):
         key += key
@@ -31,24 +13,17 @@ try:
         break
     return key
 
-  while True:
-    print ('наберите "з" или "e" чтобы зашифровать сообщение или "р" или "d" чтобы расшифровать сообщение, или "в" или "ex" для выхода')
-    cmd = input("> ")
+text = input("введите текст > ")
+try:
+    text = bytes.fromhex(text).decode('utf-8')
+    decryption = True
+except:
+    pass
 
-    if cmd in ["e", "з", "е"]:
-      text = input("введите сообщение > ")
-      key = input("введите ключ > ")
-      encrypted = encrypt(text, resize_key(key, text))
-      print (encrypted)
-    elif cmd in ["d", "р", "p"]:
-      text = input("введите шифротекст > ")
-      key = input("введите ключ > ")
-      decrypted = decrypt(text, resize_key(key, text))
-      print (decrypted)
-    elif cmd in ["ex", "в", "ех"]:
-      break
-    else:
-      print("Ошибка!")
-except Exception as err:
-  print('Произошла ошибка! отправьте на адрес "anton-tsyganenko@yandex.ru" это:')
-  print(err)
+key = input("введите ключ > ")
+result = sxor(text, resize_key(key, text))
+
+if not decryption:
+    result = "".join("{:02x}".format(ord(c)) for c in result)
+
+print (result)
