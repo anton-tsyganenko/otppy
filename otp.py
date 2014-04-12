11d00654faad134f0e778a7a5d1334c64e3516f7
Anton Tsyganenko <anton-tsyganenko@yandex.ru>
2014-04-12 18:34:43 +0400

    reworked the code, looks working now

--- otp.py
+++ otp.py
@@ -23,89 +23,127 @@
 import os
 import sys
 
-def bytesToString(bytes):
-    return "".join(chr(x) for x in bytes)
 
-def binOut(value):
-    result = ''
-    if "--bin" not in sys.argv:
-        if type(value) == bytes:
-            result += space.join('{:02x}'.format(x) for x in value)
-        if type(value) == str:
-            result += space.join('{:02x}'.format(ord(x)) for x in value)
-        return result
-    else:
-        return value
 
-def nextarg(arg): # just for better readability
-    return sys.argv[sys.argv.index(arg) + 1]
 
-if "-o" in sys.argv: # output to a file
-    def out(output):
-        outFile = open(nextarg("-o"), "w")
-        print ("output redirected to " + nextarg("-o"))
-        outFile.write(output)
-        outFile.close()
-else:
-    def out(output): print(output)
 
-if "--no-spaces" in sys.argv: # remove spaces from encrypted data and generated keys if user doesn't need them.
+#######--params
+
+fileout = False
+filein = False
+keyfile = False
+binmode = False
+genkey = False
+
+if "-o" in sys.argv:
+    fileout = True
+
+if "-i" in sys.argv:
+    filein = True
+
+if "-ki" in sys.argv:
+    keyfile = True
+
+if "--bin" in sys.argv:
+    binmode = True
+
+if "--gen-key" in sys.argv:
+    genkey = True
+
+if "--no-spaces" in sys.argv:
     space = ""
 else:
     space = " "
 
-if "--gen-key" in sys.argv: # function for keys generation
-    number = int(input("number of keys > "))
-    len = int(input("key len > "))
-    result = ""
-    for i in range(number):
-        result += binOut(bytesToString(os.urandom(len)))
-        result += "\n"
-    out(result)
-    exit()
 
-decryption = False
 
-def sxor(str1, str2): # use xor for bytes
-    return ''.join(chr(str1 ^ str2) for str1, str2 in zip(str1, str2))
+
+###############FUNCTIONS
+
+def bytesToString(bytes):
+    return "".join(chr(x) for x in bytes)
+
+def nextarg(arg): # just for better readability
+    return sys.argv[sys.argv.index(arg) + 1]
+
+def bxor(b1, b2): # use xor for bytes
+    result = b""
+    for b1, b2 in zip(b1, b2):
+        result += bytes([b1 ^ b2])
+    return result
 
 def validate_key(key, text): # don't let user to use small key
     if len(key) < len(text):
         print("the key must have the text length or be longer")
-        print(type(key), len(key), type(text), len(text))
+#        print(type(key), len(key), type(text), len(text))
         exit()
     return key
 
-if "-i" not in sys.argv:
-    text = input("enter the text > ") # input text, decide, if it's a encrypted text and convert to bytes
-else:
-    text = open(nextarg("-i"), "rb").read()
+def binOut(data):
+    if not binmode:
+        result = b""
+        result += bytes(space.join('{:02x}'.format(x) for x in data), "utf-8")
+        return result
+    else:
+        return data
 
-if "-i" in sys.argv:
-    try:
-        text = bytes.fromhex(text.replace(" ", ""))
-    except:
-        text = bytes.fromhex(bytesToString(text))
-    decryption = True
-elif "--force-encrypt" not in sys.argv:
+def out(output):
+    if fileout: # output to a file
+        with open(nextarg("-o"), "bw") as outFile:
+            outFile.write(output)
+    else:
+        print(bytesToString(output))
+
+############ rework it
+
+
+if genkey: # function for keys generation
+    number = int(input("number of keys > "))
+    len = int(input("key len > "))
+    result = ""
+    for i in range(number):
+        result += binOut(os.urandom(len))
+        result += "\n"
+    out(result)
+    exit()
+
+############/ revork it
+
+decryption = False
+
+
+################# text input
+
+if filein:
+    with open(nextarg("-i"), "rb") as file:
+        text = file.read()
+else:
+    text = input("enter the text > ")
     try:
         text = bytes.fromhex(text.replace(" ", ""))
         decryption = True
     except:
-        if "-i" not in sys.argv: text = bytes(text, "utf-8")
+        text = bytes(text, "utf-8")
+
+
+
+################# key input
+
+if keyfile:
+    with open(nextarg("-ki"), "rb") as file:
+        key = file.read()
 else:
-    text = bytes(text, "utf-8")
+    key = bytes.fromhex(input("enter the key > ").replace(" ", ""))
 
-key = input("enter the key > ") # input key, check, if it's hex
-try:
-    key = bytes.fromhex(key.replace(" ", ""))
-except:
-    print ("the key must be in HEX format")
-    exit()
 
-result = str(sxor(text, validate_key(key, text))) # encrypt/decrypt the text
+
+
+result = bxor(text, validate_key(key, text)) # encrypt/decrypt the text
 
 if not decryption: # encrypted result convert to hex format
     result = binOut(result)
 
+
 out(result)
+
+
