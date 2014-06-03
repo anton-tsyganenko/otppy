0b1aaaa0e08a6be687f464141f1ecebcd558d8c0
Anton Tsyganenko <anton-tsyganenko@yandex.ru>
2014-06-03 14:00:56 +0400

    added license

--- keygen.py
+++ keygen.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2014 Anton Tsyganenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 import sys
 import base64
+
 
 def bxor(b1, b2):            # use xor for bytes
     result = bytearray()
     for b1, b2 in zip(b1, b2):
         result.append(b1 ^ b2)
     return bytes(result)
+
 
 def randombytes(length, files):
     entropy = b""
@@ -27,7 +49,7 @@ def randombytes(length, files):
     return result
 
 files = os.listdir(sys.argv[1])
-path = sys.argv[1] + os.sep 
+path = sys.argv[1] + os.sep
 key_length = int(sys.argv[2])
 keys_number = int(sys.argv[3])
 
